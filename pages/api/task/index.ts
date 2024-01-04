46f9d8395165296b1a2a1026ad8546ad3228473a
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-01-04 18:41:46 +0530

    latest changes

--- pages/api/task/index.ts
+++ pages/api/task/index.ts
@@ -115,6 +115,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
             const client = await clientPromise;
             const db = client.db("Spak");
             const collection = db.collection<Task>("task");
+            req.body.status = 'Not Started';
             const result = await collection.insertOne(req.body);
             res.status(200).json({ data: result });
           } catch (err) {
